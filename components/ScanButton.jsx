2996bbe54c25799e21b2f2956ed4805cad2a42a2
BryanRonad <bryanronad@gmail.com>
2023-03-02 21:24:30 +0530

    feat: modal on scanButton press

--- components/ScanButton.jsx
+++ components/ScanButton.jsx
@@ -1,11 +1,60 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { useRef, useState } from "react";
+import { View, Text, TouchableOpacity, Modal } from "react-native";
 
 export default function Navbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const buttonRef = useRef(null);
+
+  const handleScanButtonPress = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleOptionButtonPress = (option) => {
+    console.log(option);
+    setShowDropdown(false);
+  };
+
   return (
-    <TouchableOpacity className="items-end justify-center">
-      <View className="w-14 h-14 rounded-full bg-main items-center justify-center shadow-sm shadow-stone-400">
-        <Text className="text-white text-4xl">+</Text>
-      </View>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        className="items-end justify-center"
+        ref={buttonRef}
+        onPress={handleScanButtonPress}
+      >
+        <View className="w-14 h-14 rounded-full bg-main items-center justify-center shadow-sm shadow-stone-400">
+          <Text className="text-white text-4xl">+</Text>
+        </View>
+      </TouchableOpacity>
+      <Modal
+        visible={showDropdown}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setShowDropdown(false)}
+      >
+        <TouchableOpacity
+          className={`flex-1 justify-center items-center bg-opacity-50 fixed`}
+          onPress={() => setShowDropdown(false)}
+        >
+          <View
+          // className="bg-white p-4 rounded-md w-full"
+          >
+            <TouchableOpacity onPress={() => handleOptionButtonPress(1)}>
+              <Text
+              //  className="text-gray-900 font-medium text-lg py-2"
+              >
+                Option 1
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleOptionButtonPress(2)}>
+              <Text
+              //  className="text-gray-900 font-medium text-lg py-2"
+              >
+                Option 2
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+      </Modal>
+    </>
   );
 }
