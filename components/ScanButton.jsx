14211d084bd3c8818ec639e0a871629ba03b6522
criston <cris10lobo@gmail.com>
2023-04-14 22:56:46 +0530

    fix: minor UI changes (FAB)

--- components/ScanButton.jsx
+++ components/ScanButton.jsx
@@ -4,61 +4,61 @@ import { styled } from "nativewind";
 const StyledView = styled(View);
 
 export default function ScanButton({ navigation }) {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const buttonRef = useRef(null);
+	const [showDropdown, setShowDropdown] = useState(false);
+	const buttonRef = useRef(null);
 
-  const handleScanButtonPress = () => {
-    setShowDropdown(!showDropdown);
-  };
+	const handleScanButtonPress = () => {
+		setShowDropdown(!showDropdown);
+	};
 
-  const handleOptionButtonPress = (option) => {
-    if (option === 1) {
-      navigation.navigate("QRScanner");
-    }
-    setShowDropdown(false);
-  };
+	const handleOptionButtonPress = (option) => {
+		if (option === 1) {
+			navigation.navigate("QRScanner");
+		}
+		setShowDropdown(false);
+	};
 
-  return (
-    <>
-      <TouchableOpacity
-        className="items-end justify-center"
-        ref={buttonRef}
-        onPress={handleScanButtonPress}
-      >
-        <View className="w-14 h-14 rounded-full bg-main items-center justify-center shadow-sm shadow-stone-400">
-          <Text className="text-white text-4xl">+</Text>
-        </View>
-      </TouchableOpacity>
-      <Modal
-        visible={showDropdown}
-        animationType="fade"
-        transparent={true}
-        onRequestClose={() => setShowDropdown(false)}
-      >
-        <TouchableOpacity
-          onPress={() => setShowDropdown(false)}
-          className={`inline-flex flex-1 justify-end items-end bg-opacity-100 fixed`}
-        >
-          <View className={`px-4 py-2 rounded-md relative -top-24`}>
-            <TouchableOpacity
-              onPress={() => handleOptionButtonPress(1)}
-              className="items-end rounded-lg my-2 bg-white border border-border shadow-sm shadow-gray-200"
-            >
-              <Text className="text-gray-900 text-base px-3 py-2">
-                Scan a QR code
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => handleOptionButtonPress(2)}
-              className="items-end rounded-lg my-2 bg-white border border-border shadow-sm shadow-gray-200"
-            >
-              <Text className="text-gray-900 text-base px-3 py-2">
-                Choose a QR image
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </TouchableOpacity>
-      </Modal>
-    </>
-  );
+	return (
+		<View className="bg-white">
+			<TouchableOpacity
+				className="items-end justify-center"
+				ref={buttonRef}
+				onPress={handleScanButtonPress}
+			>
+				<View className="w-14 h-14 rounded-full bg-main items-center justify-center shadow-sm shadow-stone-400">
+					<Text className="text-white text-4xl">+</Text>
+				</View>
+			</TouchableOpacity>
+			<Modal
+				visible={showDropdown}
+				animationType="fade"
+				transparent={true}
+				onRequestClose={() => setShowDropdown(false)}
+			>
+				<TouchableOpacity
+					onPress={() => setShowDropdown(false)}
+					className={`inline-flex flex-1 justify-end items-end bg-opacity-100 fixed`}
+				>
+					<View className={`px-4 py-2 rounded-md relative -top-24`}>
+						<TouchableOpacity
+							onPress={() => handleOptionButtonPress(1)}
+							className="items-end rounded-lg my-2 bg-white border border-border shadow-sm shadow-gray-200"
+						>
+							<Text className="text-gray-900 text-base px-3 py-2">
+								Scan a QR code
+							</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => handleOptionButtonPress(2)}
+							className="items-end rounded-lg my-2 bg-white border border-border shadow-sm shadow-gray-200"
+						>
+							<Text className="text-gray-900 text-base px-3 py-2">
+								Choose a QR image
+							</Text>
+						</TouchableOpacity>
+					</View>
+				</TouchableOpacity>
+			</Modal>
+		</View>
+	);
 }
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -6,51 +6,51 @@ import Device from "../components/Device";
 import ScanButton from "../components/ScanButton";
 
 const HomeScreen = ({ navigation }) => {
-  const [deviceArr, setDeviceArr] = useState([
-    {
-      name: "Device 1",
-      lastLogin: "2021-05-01 12:00:00",
-      location: "Mumbai, IN",
-    },
-    {
-      name: "Device 2",
-      lastLogin: "2021-05-01 12:00:00",
-      location: "Nashik, IN",
-    },
-    {
-      name: "Device 3",
-      lastLogin: "2021-05-01 12:00:00",
-      location: "Pune, IN",
-    },
-  ]);
-  return (
-    <>
-      <SafeAreaView className="h-full flex-col items-center">
-        <View className="flex-none p-5 w-full justify-center bg-white border-y-2 border-border">
-          <Navbar />
-        </View>
+	const [deviceArr, setDeviceArr] = useState([
+		{
+			name: "Device 1",
+			lastLogin: "2021-05-01 12:00:00",
+			location: "Mumbai, IN",
+		},
+		{
+			name: "Device 2",
+			lastLogin: "2021-05-01 12:00:00",
+			location: "Nashik, IN",
+		},
+		{
+			name: "Device 3",
+			lastLogin: "2021-05-01 12:00:00",
+			location: "Pune, IN",
+		},
+	]);
+	return (
+		<>
+			<SafeAreaView className="h-full flex-col items-center">
+				<View className="flex-none p-5 w-full justify-center bg-white border-y-2 border-border">
+					<Navbar />
+				</View>
 
-        <View className="grow w-full items-center bg-white">
-          <View className="w-full">
-            {deviceArr.map((device, index) => (
-              <Device
-                key={index}
-                name={device.name}
-                lastLogin={device.lastLogin}
-                location={device.location}
-              />
-            ))}
-          </View>
-        </View>
+				<View className="grow w-full items-center bg-white">
+					<View className="w-full">
+						{deviceArr.map((device, index) => (
+							<Device
+								key={index}
+								name={device.name}
+								lastLogin={device.lastLogin}
+								location={device.location}
+							/>
+						))}
+					</View>
+				</View>
 
-        <View className="flex-none px-4 py-2 w-full">
-          <ScanButton navigation={navigation} />
-        </View>
+				<View className="flex-none px-8 py-8 w-full bg-white">
+					<ScanButton navigation={navigation} />
+				</View>
 
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </>
-  );
+				<StatusBar style="auto" />
+			</SafeAreaView>
+		</>
+	);
 };
 
 export default HomeScreen;
