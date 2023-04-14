ecd2d1d354bde656340290253db238556e57da81
criston <cris10lobo@gmail.com>
2023-04-14 22:52:07 +0530

    feat: added loader and navigation to home

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  StyleSheet,
-  Keyboard,
+	View,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	TouchableWithoutFeedback,
+	StyleSheet,
+	Keyboard,
 } from "react-native";
 import signal, { KeyHelper } from "signal-protocol-react-native";
 import signalStore from "../util/signalStore.js";
@@ -17,198 +17,199 @@ import { AuthContext } from "../context/AuthContext";
 import { randomUUID } from "expo-crypto";
 
 const LoginScreen = ({ navigation }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginError, setLoginError] = useState(false);
-  const { setIsAuthenticated } = React.useContext(AuthContext);
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [loginError, setLoginError] = useState(false);
+	const { setIsAuthenticated } = React.useContext(AuthContext);
 
-  const generateIdentity = async (store) => {
-    const result = await Promise.all([
-      KeyHelper.generateIdentityKeyPair(),
-      KeyHelper.generateRegistrationId(),
-    ]);
-    return result;
-  };
+	const generateIdentity = async (store) => {
+		const result = await Promise.all([
+			KeyHelper.generateIdentityKeyPair(),
+			KeyHelper.generateRegistrationId(),
+		]);
+		return result;
+	};
 
-  const generatePreKeyBundle = async (store, preKeyId, signedPreKeyId) => {
-    const result = await Promise.all([
-      store.getIdentityKeyPair(),
-      store.getLocalRegistrationId(),
-    ]);
-    var identityKey = result[0];
-    var registrationId = result[1];
-    const keys = await Promise.all([
-      KeyHelper.generatePreKey(preKeyId),
-      KeyHelper.generateSignedPreKey(identityKey, signedPreKeyId),
-    ]);
-    var preKey = keys[0];
-    var signedPreKey = keys[1];
-    await store.storePreKey(preKeyId, preKey.keyPair);
-    await store.storeSignedPreKey(signedPreKeyId, signedPreKey.keyPair);
-    return {
-      identityKey: Buffer.from(identityKey.pubKey).toString("base64"),
-      registrationId: registrationId,
-      preKey: {
-        keyId: preKeyId,
-        publicKey: Buffer.from(preKey.keyPair.pubKey).toString("base64"),
-      },
-      signedPreKey: {
-        keyId: signedPreKeyId,
-        publicKey: Buffer.from(signedPreKey.keyPair.pubKey).toString("base64"),
-        signature: Buffer.from(signedPreKey.signature).toString("base64"),
-      },
-    };
-  };
+	const generatePreKeyBundle = async (store, preKeyId, signedPreKeyId) => {
+		const result = await Promise.all([
+			store.getIdentityKeyPair(),
+			store.getLocalRegistrationId(),
+		]);
+		var identityKey = result[0];
+		var registrationId = result[1];
+		const keys = await Promise.all([
+			KeyHelper.generatePreKey(preKeyId),
+			KeyHelper.generateSignedPreKey(identityKey, signedPreKeyId),
+		]);
+		var preKey = keys[0];
+		var signedPreKey = keys[1];
+		await store.storePreKey(preKeyId, preKey.keyPair);
+		await store.storeSignedPreKey(signedPreKeyId, signedPreKey.keyPair);
+		return {
+			identityKey: Buffer.from(identityKey.pubKey).toString("base64"),
+			registrationId: registrationId,
+			preKey: {
+				keyId: preKeyId,
+				publicKey: Buffer.from(preKey.keyPair.pubKey).toString("base64"),
+			},
+			signedPreKey: {
+				keyId: signedPreKeyId,
+				publicKey: Buffer.from(signedPreKey.keyPair.pubKey).toString("base64"),
+				signature: Buffer.from(signedPreKey.signature).toString("base64"),
+			},
+		};
+	};
 
-  const handleLogin = async () => {
-    // TODO: Implement login logic here
-    // Check if username and password match statically
-    if (
-      username.toLowerCase() !== "test" ||
-      password.toLowerCase() !== "test"
-    ) {
-      setLoginError(true);
-      return;
-    }
+	const handleLogin = async () => {
+		// TODO: Implement login logic here
+		// Check if username and password match statically
+		if (
+			username.toLowerCase() !== "test" ||
+			password.toLowerCase() !== "test"
+		) {
+			setLoginError(true);
+			return;
+		}
 
-    // Clear login error
-    setLoginError(false);
+		// Clear login error
+		setLoginError(false);
 
-    // Create signal protocol address object
-    const signalProtocolAddress = new signal.SignalProtocolAddress(username, 1);
+		// // Create signal protocol address object
+		// const signalProtocolAddress = new signal.SignalProtocolAddress(username, 1);
 
-    // Set preKeyId and signedPreKeyId
-    const preKeyId = 1337;
-    const signedPreKeyId = 1;
-    // Call the generateIdentity function to generate a new identity key pair
-    const [identityKey, registrationId] = await generateIdentity(signalStore);
-    // Save the identity key pair and registration id in the signal store
-    await signalStore.saveIdentity(
-      signalProtocolAddress.toString(),
-      identityKey
-    );
-    signalStore.put("registrationId", registrationId);
-    signalStore.put("identityKey", identityKey);
-    // Call the generatePreKeyBundle function to generate a new pre key bundle
-    const preKeyBundle = await generatePreKeyBundle(
-      signalStore,
-      preKeyId,
-      signedPreKeyId
-    );
-    console.log("Pre Key Bundle: ", preKeyBundle);
+		// // Set preKeyId and signedPreKeyId
+		// const preKeyId = 1337;
+		// const signedPreKeyId = 1;
+		// // Call the generateIdentity function to generate a new identity key pair
+		// const [identityKey, registrationId] = await generateIdentity(signalStore);
+		// // Save the identity key pair and registration id in the signal store
+		// await signalStore.saveIdentity(
+		//   signalProtocolAddress.toString(),
+		//   identityKey
+		// );
+		// signalStore.put("registrationId", registrationId);
+		// signalStore.put("identityKey", identityKey);
+		// // Call the generatePreKeyBundle function to generate a new pre key bundle
+		// const preKeyBundle = await generatePreKeyBundle(
+		//   signalStore,
+		//   preKeyId,
+		//   signedPreKeyId
+		// );
+		// console.log("Pre Key Bundle: ", preKeyBundle);
 
-    // Alert with all information
-    alert(
-      `Username: ${username}\n
-      Signal Protocol Address: ${signalProtocolAddress.toString()}\n
-      Pre Key Bundle Successfully created..\n
-      Sending Pre Key Bundle to server...\n
-      Successfully registered user on server!
-      `
-    );
+		// // Alert with all information
+		// alert(
+		//   `Username: ${username}\n
+		//   Signal Protocol Address: ${signalProtocolAddress.toString()}\n
+		//   Pre Key Bundle Successfully created..\n
+		//   Sending Pre Key Bundle to server...\n
+		//   Successfully registered user on server!
+		//   `
+		// );
 
-    // Send preKeyBundle to server
-    fetch(`http://192.168.29.215:5000/api/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username,
-        signalProtocolAddress: signalProtocolAddress.toString(),
-        preKeyBundle,
-      }),
-    }).then((res) => {
-      if (res.status === 200) {
-        console.log("User successfully logged in");
-        // Store username, signalProtocolAddress and IdentityKey in secure storage
-        SecureStore.setItemAsync("username", username);
-        SecureStore.setItemAsync(
-          "signalProtocolAddress",
-          signalProtocolAddress.toString()
-        );
-        SecureStore.setItemAsync(
-          "identityKey",
-          Buffer.from(preKeyBundle.identityKey).toString("base64")
-        );
-        // Set user as authenticated
-        setIsAuthenticated(true);
-      } else {
-        console.log("User login failed");
-      }
-    });
-  };
+		// // Send preKeyBundle to server
+		// fetch(`http://192.168.29.215:5000/api/auth/login`, {
+		//   method: "POST",
+		//   headers: {
+		//     "Content-Type": "application/json",
+		//   },
+		//   body: JSON.stringify({
+		//     username,
+		//     signalProtocolAddress: signalProtocolAddress.toString(),
+		//     preKeyBundle,
+		//   }),
+		// }).then((res) => {
+		//   if (res.status === 200) {
+		//     console.log("User successfully logged in");
+		//     // Store username, signalProtocolAddress and IdentityKey in secure storage
+		//     SecureStore.setItemAsync("username", username);
+		//     SecureStore.setItemAsync(
+		//       "signalProtocolAddress",
+		//       signalProtocolAddress.toString()
+		//     );
+		//     SecureStore.setItemAsync(
+		//       "identityKey",
+		//       Buffer.from(preKeyBundle.identityKey).toString("base64")
+		//     );
+		//     // Set user as authenticated
+		//     setIsAuthenticated(true);
+		//   } else {
+		//     console.log("User login failed");
+		//   }
+		// });
+		setIsAuthenticated(true);
+	};
 
-  const handlePress = () => {
-    Keyboard.dismiss(); // Hide the keyboard when user clicks outside of the text field
-  };
+	const handlePress = () => {
+		Keyboard.dismiss(); // Hide the keyboard when user clicks outside of the text field
+	};
 
-  return (
-    <TouchableWithoutFeedback onPress={handlePress}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Welcome to FDrive!</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry={true}
-          value={password}
-          onChangeText={setPassword}
-        />
-        <TouchableOpacity
-          className="bg-main"
-          style={styles.button}
-          onPress={handleLogin}
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
-        {loginError && (
-          <Text className=" text-red-500 mt-5">
-            Incorrect email or password
-          </Text>
-        )}
-      </View>
-    </TouchableWithoutFeedback>
-  );
+	return (
+		<TouchableWithoutFeedback onPress={handlePress}>
+			<View style={styles.container}>
+				<Text style={styles.title}>Welcome to FDrive!</Text>
+				<TextInput
+					style={styles.input}
+					placeholder="Username"
+					value={username}
+					onChangeText={setUsername}
+				/>
+				<TextInput
+					style={styles.input}
+					placeholder="Password"
+					secureTextEntry={true}
+					value={password}
+					onChangeText={setPassword}
+				/>
+				<TouchableOpacity
+					className="bg-main"
+					style={styles.button}
+					onPress={handleLogin}
+				>
+					<Text style={styles.buttonText}>Login</Text>
+				</TouchableOpacity>
+				{loginError && (
+					<Text className=" text-red-500 mt-5">
+						Incorrect email or password
+					</Text>
+				)}
+			</View>
+		</TouchableWithoutFeedback>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  input: {
-    width: "80%",
-    height: 50,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-  },
-  button: {
-    width: "80%",
-    height: 50,
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+		marginBottom: 20,
+	},
+	input: {
+		width: "80%",
+		height: 50,
+		borderWidth: 1,
+		borderColor: "#ccc",
+		borderRadius: 10,
+		paddingHorizontal: 10,
+		marginBottom: 20,
+	},
+	button: {
+		width: "80%",
+		height: 50,
+		borderRadius: 10,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	buttonText: {
+		color: "white",
+		fontSize: 18,
+		fontWeight: "bold",
+	},
 });
 
 export default LoginScreen;
--- screens/QRScannerScreen.jsx
+++ screens/QRScannerScreen.jsx
@@ -7,176 +7,181 @@ import * as SecureStore from "expo-secure-store";
 import signal from "signal-protocol-react-native";
 import signalStore from "../util/signalStore.js";
 import { Buffer } from "buffer";
+import { ActivityIndicator } from "react-native";
 
 const QRScannerScreen = ({ navigation }) => {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [data, setData] = useState(
-    " Open web.whatapp.com on your browser or any device and scan the QRCode."
-  );
+	const [hasPermission, setHasPermission] = useState(null);
+	const [scanned, setScanned] = useState(false);
+	const [showLoader, setShowLoader] = useState(false);
+	const [data, setData] = useState(
+		" Open the vault page on FDrive and scan the QR Code. "
+	);
 
-  const askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  };
+	const askForCameraPermission = () => {
+		(async () => {
+			const { status } = await BarCodeScanner.requestPermissionsAsync();
+			setHasPermission(status === "granted");
+		})();
+	};
 
-  // Request Camera Permission
-  useEffect(() => {
-    askForCameraPermission();
-  }, []);
+	// Request Camera Permission
+	useEffect(() => {
+		askForCameraPermission();
+	}, []);
 
-  // Scan QRCode
-  const handleScan = async ({ type, data }) => {
-    setData(data);
-    const token = data.toString();
-    console.log("Scan successful: ", token);
-    // Check if data is a valid token
-    const hexKeyRegex = /^[0-9a-fA-F]{32}$/;
-    if (hexKeyRegex.test(token)) {
-      setScanned(true);
-      // Store identityKey of companion in secure store
-      let companions = await SecureStore.getItemAsync("companions");
-      if (!companions) companions = JSON.stringify([]);
-      const updatedCompanions = [...JSON.parse(companions), token];
-      await SecureStore.setItemAsync(
-        "companions",
-        JSON.stringify(updatedCompanions)
-      );
-      // Verify token with server
-      fetch(`http://192.168.29.215:5000/api/session/${token}/verify`, {
-        method: "GET",
-      }).then((res) => {
-        if (res.status === 200) {
-          console.log("Token verified by server");
-          // Websocket connection to path /primary/:token
-          const socket = new WebSocket(
-            `ws://192.168.29.215:5000/primary/${token}`
-          );
-          socket.onopen = () => {
-            console.log("Socket connection established");
-          };
-          socket.onmessage = async (event) => {
-            // Determine type of message
-            const receivedMessage = JSON.parse(event.data);
-            if (receivedMessage.type === "greeting") {
-              console.log(`Server says: ${receivedMessage.message}`);
-            }
-            if (receivedMessage.type === "preKeyWhisperMessage") {
-              const address = await SecureStore.getItemAsync(
-                "signalProtocolAddress"
-              );
-              const sessionCipher = new signal.SessionCipher(
-                signalStore,
-                signal.SignalProtocolAddress.fromString(address)
-              );
-              sessionCipher
-                .decryptPreKeyWhisperMessage(
-                  receivedMessage.preKeyWhisperMessage.body,
-                  "binary"
-                )
-                .then((plaintext) => {
-                  console.log(
-                    "Decryption successful",
-                    Buffer.from(plaintext).toString("utf8")
-                  );
-                  console.log(sessionCipher);
-                  socket.send(
-                    JSON.stringify({
-                      type: "X3DHCompletionMessage",
-                      data: {
-                        startTime: new Date(),
-                      },
-                    })
-                  );
-                })
-                .catch((err) => console.log("Failed to decrypt", err));
-              return;
-            }
-            // Send signal related information to server
-            const message = {
-              type: "initialPrimaryX3DHMessage",
-              username: await SecureStore.getItemAsync("username"),
-              signalProtocolAddress: await SecureStore.getItemAsync(
-                "signalProtocolAddress"
-              ),
-              deviceDetails: "Test Device, details",
-            };
-            socket.send(JSON.stringify(message));
-          };
-          socket.onerror = (error) => {
-            console.log(`Socket encountered error:`, error);
-          };
-          socket.onclose = (event) => {
-            if (event.wasClean) {
-              console.log("Socket connection closed cleanly");
-            }
-            console.log(`Socket connection closed with code: ${event.code}`);
-          };
-        } else {
-          console.log("Token not verified", res.status);
-        }
-      });
-    } else {
-      console.log("Invalid token");
-    }
-  };
+	// Scan QRCode
+	const handleScan = async ({ type, data }) => {
+		setScanned(true); //REMOVE
+		setShowLoader(true); //ADD during fetch
 
-  // Screen when system permission modal is shown
-  if (hasPermission === null) {
-    return (
-      <View className="flex flex-col h-full justify-between items-center bg-white">
-        <View className="m-3 p-4 bg-gray-100 rounded">
-          <Text className="text-center text-md">
-            Open web.whatapp.com on your browser or any device and scan the QR
-            Code.
-          </Text>
-        </View>
-        <View className=" p-4 h-full w-screen bg-red-100 rounded"></View>
-      </View>
-    );
-  }
+		setData(data);
 
-  // Screen when permission is denied
-  if (hasPermission === false) {
-    return (
-      <NoPermissionModal
-        navigation={navigation}
-        askForCameraPermission={askForCameraPermission}
-      />
-      // <View>
-      //   <Button
-      //     onPress={() => askForCameraPermission()}
-      //     title="click me"
-      //   ></Button>
-      // </View>
-    );
-  }
+		navigation.navigate("Home"); //ADD after successfull fetch
 
-  return (
-    <View className="flex flex-col h-full justify-between items-center bg-white">
-      <View className="m-3 p-4 bg-gray-100 rounded">
-        <Text className="text-center text-md">
-          {data}{" "}
-          {scanned && (
-            <Button title="scan again" onPress={() => setScanned(false)} />
-          )}
-        </Text>
-      </View>
-      <View className=" h-full bg-red-100 rounded">
-        {/* <BarCodeScanner
+		const token = data.toString();
+		console.log("Scan successful: ", token);
+		// Check if data is a valid token
+		const hexKeyRegex = /^[0-9a-fA-F]{32}$/;
+		if (hexKeyRegex.test(token)) {
+			// setScanned(true);
+			// Store identityKey of companion in secure store
+			let companions = await SecureStore.getItemAsync("companions");
+			if (!companions) companions = JSON.stringify([]);
+			const updatedCompanions = [...JSON.parse(companions), token];
+			await SecureStore.setItemAsync(
+				"companions",
+				JSON.stringify(updatedCompanions)
+			);
+			// Verify token with server
+			fetch(`http://192.168.29.215:5000/api/session/${token}/verify`, {
+				method: "GET",
+			}).then((res) => {
+				if (res.status === 200) {
+					console.log("Token verified by server");
+					// Websocket connection to path /primary/:token
+					const socket = new WebSocket(
+						`ws://192.168.29.215:5000/primary/${token}`
+					);
+					socket.onopen = () => {
+						console.log("Socket connection established");
+					};
+					socket.onmessage = async (event) => {
+						// Determine type of message
+						const receivedMessage = JSON.parse(event.data);
+						if (receivedMessage.type === "greeting") {
+							console.log(`Server says: ${receivedMessage.message}`);
+						}
+						if (receivedMessage.type === "preKeyWhisperMessage") {
+							const address = await SecureStore.getItemAsync(
+								"signalProtocolAddress"
+							);
+							const sessionCipher = new signal.SessionCipher(
+								signalStore,
+								signal.SignalProtocolAddress.fromString(address)
+							);
+							sessionCipher
+								.decryptPreKeyWhisperMessage(
+									receivedMessage.preKeyWhisperMessage.body,
+									"binary"
+								)
+								.then((plaintext) => {
+									console.log(
+										"Decryption successful",
+										Buffer.from(plaintext).toString("utf8")
+									);
+									console.log(sessionCipher);
+									socket.send(
+										JSON.stringify({
+											type: "X3DHCompletionMessage",
+											data: {
+												startTime: new Date(),
+											},
+										})
+									);
+								})
+								.catch((err) => console.log("Failed to decrypt", err));
+							return;
+						}
+						// Send signal related information to server
+						const message = {
+							type: "initialPrimaryX3DHMessage",
+							username: await SecureStore.getItemAsync("username"),
+							signalProtocolAddress: await SecureStore.getItemAsync(
+								"signalProtocolAddress"
+							),
+							deviceDetails: "Test Device, details",
+						};
+						socket.send(JSON.stringify(message));
+					};
+					socket.onerror = (error) => {
+						console.log(`Socket encountered error:`, error);
+					};
+					socket.onclose = (event) => {
+						if (event.wasClean) {
+							console.log("Socket connection closed cleanly");
+						}
+						console.log(`Socket connection closed with code: ${event.code}`);
+					};
+				} else {
+					console.log("Token not verified", res.status);
+				}
+			});
+		} else {
+			console.log("Invalid token");
+		}
+	};
+
+	// Screen when system permission modal is shown
+	if (hasPermission === null) {
+		return (
+			<View className="flex flex-col h-full justify-between items-center bg-white">
+				<View className="m-3 p-4 bg-gray-100 rounded">
+					<Text className="text-center text-md">
+						Open web.whatapp.com on your browser or any device and scan the QR
+						Code.
+					</Text>
+				</View>
+				<View className=" p-4 h-full w-screen bg-red-100 rounded"></View>
+			</View>
+		);
+	}
+
+	// Screen when permission is denied
+	if (hasPermission === false) {
+		return (
+			<NoPermissionModal
+				navigation={navigation}
+				askForCameraPermission={askForCameraPermission}
+			/>
+			// <View>
+			//   <Button
+			//     onPress={() => askForCameraPermission()}
+			//     title="click me"
+			//   ></Button>
+			// </View>
+		);
+	}
+
+	return (
+		<View className="flex flex-col h-full justify-between items-center bg-white">
+			<View className="m-3 p-4 bg-gray-100 rounded">
+				<Text className="text-center text-md">
+					{scanned ? <ActivityIndicator /> : data}
+				</Text>
+			</View>
+			<View className=" h-full bg-red-100 rounded">
+				{/* <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleScan}
           style={{ width: 900, height: 1600 }}
         /> */}
-        <Camera
-          onBarCodeScanned={scanned ? undefined : handleScan}
-          ratio="16:9"
-          style={{ width: 450, height: 854 }}
-        />
-      </View>
-    </View>
-  );
+				<Camera
+					onBarCodeScanned={scanned ? undefined : handleScan}
+					ratio="16:9"
+					style={{ width: 450, height: 854 }}
+				/>
+			</View>
+		</View>
+	);
 };
 
 export default QRScannerScreen;
